d48f751aea792fe8867867d23d500097d4ace31c
ggomagundan <pbs52@hanmail.net>
2015-04-03 21:41:44 +0900

    d

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, "434707570036416", "1d2b3386433bc284d7dd3915f9a966f8",
-      :scope => 'email,user_birthday,read_stream,adaccounts{account_id,account_groups,account_status,amount_spent,age,business_name,business_state,business,business_city,currency,daily_spend_limit}', :display => 'popup'
+      :scope => 'email,user_birthday,read_stream,adaccounts', :display => 'popup'
 end
